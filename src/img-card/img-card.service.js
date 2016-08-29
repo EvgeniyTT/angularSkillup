33f4c3d2dd162045441783e4821d06b787ac0e5a
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2016-08-29 13:53:12 +0300

    update server

--- src/img-card/img-card.service.js
+++ src/img-card/img-card.service.js
@@ -2,14 +2,45 @@ export default class imgService {
   constructor($http) {
     'ngInject';
     this.$http = $http;
+    // this.url = 'http://localhost:3001';
+    this.url = 'http://10.10.54.24:3001';
   }
 
+  // JSON-SERVER
+  // add(img) {
+  //   return this.$http.post(`${this.url}/imgs`, img);
+  // }
+  //
+  // update(id, imgData) {
+  //   return this.$http.put(`${this.url}/imgs/${id}`, imgData);
+  // }
+  //
+  // save(img) {
+  //   if (img.id) {
+  //     return this.update(img.id, img);
+  //   }
+  //   return this.add(img);
+  // }
+  //
+  // remove(id) {
+  //   return this.$http.delete(`${this.url}/imgs/${id}`);
+  // }
+  //
+  // list() {
+  //   return this.$http.get(`${this.url}/imgs`);
+  // }
+  //
+  // get(id) {
+  //   return this.$http.get(`${this.url}/${id}`);
+  // }
+
+  // NODE
   add(img) {
-    return this.$http.post('http://10.10.54.24:3000/imgs', img);
+    return this.$http.post(`${this.url}/`, img);
   }
 
   update(id, imgData) {
-    return this.$http.put(`http://10.10.54.24:3000/imgs/${id}`, imgData);
+    return this.$http.put(`${this.url}/${id}`, imgData);
   }
 
   save(img) {
@@ -20,14 +51,15 @@ export default class imgService {
   }
 
   remove(id) {
-    return this.$http.delete(`http://10.10.54.24:3000/imgs/${id}`);
+    return this.$http.delete(`${this.url}/${id}`);
   }
 
   list() {
-    return this.$http.get('http://10.10.54.24:3000/imgs');
+    return this.$http.get(`${this.url}/allImages`);
   }
 
   get(id) {
-    return this.$http.get(`http://10.10.54.24:3000/imgs/${id}`);
+    return this.$http.get(`${this.url}/${id}`);
   }
+
 }
--- src/server.js
+++ src/server.js
@@ -4,59 +4,87 @@ const mongodb = require('mongodb');
 const express = require('express');
 const assert = require('assert');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 function errorHandler(err, req, res, next) {
   res.status(500);
-  res.send(err);
+  res.send('error', { error: err });
 }
 
+const host = '10.10.54.24';
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use(errorHandler);
 
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/imageGallery', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/imageGallery', function (err, db) {
   assert.equal(err, null);
   let maxId;
-  db.collection('images').find().sort({_id:-1}).limit(1).toArray(function(err, imageWithMaxId) {
-    maxId = imageWithMaxId.id;
-  });
-
+  db.collection('images').find().sort({ id: -1 }).limit(1)
+    .toArray( function(err, imageWithMaxId) {
+      maxId = imageWithMaxId[0].id;
+    });
 
   app.get('/allImages', function (req, res) {
     db.collection('images').find().toArray(function(err, images) {
       assert.equal(err, null);
       res.send(images);
-    })
+    });
   });
 
-  app.get('/details/:id', function (req, res, next) {
-    const id = parseInt(req.params.id);
-    if(isNan(id)) {
+  app.get('/:imageId', function (req, res, next) {
+    const imageId = parseInt(req.params.imageId, 10);
+    if (isNaN(imageId)) {
       next(Error('can`t parse image id to number'));
+    } else {
+      db.collection('images').findOne({ id: imageId }, function(err, image) {
+        assert.equal(null, err);
+        res.send(image);
+      });
     }
-    db.collection('images').find({ id: id }).toArray(function(err, image) {
-      assert.equal(err, null);
-      res.send(image);
-    })
   });
 
   app.post('/', function (req, res, next) {
-    const image = req.body.image;
+    const image = req.body;
     if (image.src == 'undefined' || image.description == 'undefined') {
       next(Error('Required fields are not populated'));
     } else {
-      image.id = maxId + 1;
-      db.collection('images').insert(image).toArray(function(err, result) {
+      image.id = maxId++;
+      db.collection('images').insert(image, function(err, result) {
+        assert.equal(err, null);
+        res.send('Image was successfully inserted');
+      });
+    }
+  });
+
+  app.put('/', function(req, res, next) {
+    const image = req.body;
+    if (image.src == 'undefined' || image.description == 'undefined') {
+      next(Error('Required fields are not populated'));
+    } else {
+      db.collection('images').update({ id: image.id }, image, function(err, result) {
         assert.equal(err, null);
         res.send('Image was successfully inserted');
         maxId++;
-      })
-    };
+      });
+    }
+  });
+
+  app.delete('/:imageId', function (req, res, next) {
+    const imageId = parseInt(req.params.imageId, 10);
+    if (isNaN(imageId)) {
+      next(Error('can`t parse image id to number'));
+    } else {
+      db.collection('images').remove({ id: imageId }, function(err) {
+        assert.equal(err, null);
+        res.send('image was deleted successfully');
+      });
+    }
   });
 });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
-})
+app.listen(3001, host,  function () {
+  console.log('app listening on port 3001!');
+});
